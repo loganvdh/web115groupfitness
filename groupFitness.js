cd3a8047ced66c133bb3bbe5a0bad94e0e7a92a6
loganvdh <56613853+loganvdh@users.noreply.github.com>
2019-10-15 20:33:23 -0400

    Update groupFitness.js

--- groupFitness.js
+++ groupFitness.js
@@ -9,6 +9,21 @@ function inputSubmit(){
 		var meal = document.getElementById(i).value;
 		tuesday.push(meal);
 	}
+	var wednesday = [];
+	for (i=10; i < 15; i++){
+		var meal = document.getElementById(i).value;
+		wednesday.push(meal);
+	}
+	var thursday = [];
+	for (i=15; i < 20; i++){
+		var meal = document.getElementById(i).value;
+		thursday.push(meal);
+	}
+	var friday = [];
+	for (i=20; i < 25; i++){
+		var meal = document.getElementById(i).value;
+		monday.push(meal);
+	}
 	console.log(monday);
 	console.log(tuesday);
 }
