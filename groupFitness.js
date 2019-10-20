f4e309cd6cac976bdf60c4af41a1036d0a874d02
Mattmn <mattn6206@gmail.com>
2019-10-20 14:12:24 -0400

    simple working list, modified print function
    The inputSubmit function now creates a simple list of meals by weekday. Also, I changed the print() to _print() to fix an endless loop error. The print function also hides the buttons when printing.

--- groupFitness.js
+++ groupFitness.js
@@ -22,10 +22,43 @@ function inputSubmit(){
 	var friday = [];
 	for (i=20; i < 25; i++){
 		var meal = document.getElementById(i).value;
-		monday.push(meal);
+		friday.push(meal);
 	}
-	console.log(monday);
-	console.log(tuesday);
+	var saturday = [];
+	for (i=25; i < 30; i++){
+		var meal = document.getElementById(i).value;
+		saturday.push(meal);
+	}
+	var sunday = [];
+	for (i=30; i < 35; i++){
+		var meal = document.getElementById(i).value;
+		sunday.push(meal);
+	}
+    
+	var weekdayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", 
+                        "Friday", "Saturday", "Sunday"];
+    var weekdaySchedules = [monday, tuesday, wednesday, thursday,
+                            friday, saturday, sunday];
+    var mealnames = ["Breakfast", "Snack 1", "Lunch", "Snack 2", "Dinner"];
+                            
+    // creating an unordered list of weekdays
+    document.write("<ul>");
+    for(i=0; i < weekdayNames.length; i++){
+        document.write("<p><li><b>" + weekdayNames[i] + "</b>");
+        
+        // creating an inner unordered list of meals
+        document.write("<ul>");
+        for (j=0; j < mealnames.length; j++){
+            if (weekdaySchedules[i][j] != ""){ // ignoring empty meal values
+                document.write("<li>");
+                document.write(mealnames[j] + ": " + weekdaySchedules[i][j]);
+                document.write("</li>");
+            }
+        }
+        document.write("</ul>");
+        document.write("</li></p>");
+    }
+    document.write("</ul>");
 }
 
 function textclear(){
@@ -34,6 +67,10 @@ function textclear(){
     }
 }
 
-function print(){
+function _print(){
+    // toggle button visibility when printing
+    var buttons = document.getElementById("buttons");
+    buttons.style.display = "none";
     window.print();
+    buttons.style.display = "block";
 }
